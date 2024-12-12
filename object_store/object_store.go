e8201d77d5ffce901c7a8d5ba4a16d62c11b8d5d
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-12-12 15:36:01 +0100

    stub s3 interface
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- object_store/object_store.go
+++ object_store/object_store.go
@@ -2,9 +2,12 @@ package objectstore
 
 import (
 	"context"
+	"io"
 
-	"gocloud.dev/blob/s3blob"
-	//   "gocloud.dev/blob/s3blob"
+	"fmt"
+
+	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/s3blob"
 )
 
 /*
@@ -12,6 +15,7 @@ provides a mapping between the filesystem object, the inode, and the
 dir object with the blob?
 */
 
+/*
 func NewBucket() {
 	ctx = context.Background()
 	bucket, err := s3blob.OpenBucket(ctx, sess, "mybucket")
@@ -19,4 +23,44 @@ func NewBucket() {
 	if err != nil {
 		return bucket
 	}
+}
+*/
+
+func UploadObject(ctx context.Context, bucket *blob.Bucket, key string, data []byte) error {
+	w, err := bucket.NewWriter(ctx, key, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create writer: %v", err)
+	}
+	defer w.Close()
+
+	_, err = w.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to write data: %v", err)
+	}
+
+	return nil
+}
+
+func DownloadObject(ctx context.Context, bucket *blob.Bucket, key string) ([]byte, error) {
+	r, err := bucket.NewReader(ctx, key, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create reader: %v", err)
+	}
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read data: %v", err)
+	}
+
+	return data, nil
+}
+
+func DeleteObject(ctx context.Context, bucket *blob.Bucket, key string) error {
+	err := bucket.Delete(ctx, key)
+	if err != nil {
+		return fmt.Errorf("failed to delete object: %v", err)
+	}
+
+	return nil
 }
