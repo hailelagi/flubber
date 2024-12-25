4a897c5b435b20204c726e90c180dd3d272f8e1b
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-12-25 03:12:44 +0100

    stub open
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- fuse/file_system.go
+++ fuse/file_system.go
@@ -16,7 +16,7 @@ type HelloRoot struct {
 func (r *HelloRoot) OnAdd(ctx context.Context) {
 	ch := r.NewPersistentInode(
 		ctx, &fs.MemRegularFile{
-			Data: []byte("file.txt"),
+			Data: []byte("Hello, world!\n"),
 			Attr: fuse.Attr{
 				Mode: 0644,
 			},
@@ -29,8 +29,24 @@ func (r *HelloRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.Att
 	return 0
 }
 
+func (r *HelloRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	return nil, fuse.FOPEN_KEEP_CACHE, 0
+}
+
+func (r *HelloRoot) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	data := []byte("Hello, world!\n")
+	end := off + int64(len(dest))
+	if end > int64(len(data)) {
+		end = int64(len(data))
+	}
+	return fuse.ReadResultData(data[off:end]), 0
+}
+
 var _ = (fs.NodeGetattrer)((*HelloRoot)(nil))
 var _ = (fs.NodeOnAdder)((*HelloRoot)(nil))
+var _ = (fs.NodeOpener)((*HelloRoot)(nil))
+
+//var _ = (fs.NodeReader)((*HelloRoot)(nil))
 
 func Mount(dir string) error {
 	debug := flag.Bool("debug", true, "print debug data")
