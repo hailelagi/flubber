9ed004e39cb04a111644b03fc3baa237228d0288
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2025-01-06 11:09:09 +0100

    feat: parent and root traversal
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- internal/fuse/file_system.go
+++ internal/fuse/file_system.go
@@ -9,31 +9,52 @@ import (
 	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
-type HelloRoot struct {
+type flubberRoot struct {
 	fs.Inode
 }
 
-func (r *HelloRoot) OnAdd(ctx context.Context) {
+/*
+todo move to during new file/inode allocation/creation
+func (r *flubberRoot) OnAdd(ctx context.Context) {
 	ch := r.NewPersistentInode(
 		ctx, &fs.MemRegularFile{
 			Data: []byte("Hello, world!\n"),
 			Attr: fuse.Attr{
-				Mode: 0644,
+				Mode: 0o644,
 			},
 		}, fs.StableAttr{Ino: 2})
 	r.AddChild("file.txt", ch, false)
 }
+*/
 
-func (r *HelloRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+// on mount, create path traversal and hello.txt
+func (r *flubberRoot) OnAdd(ctx context.Context) {
+	r.AddChild(".", r.EmbeddedInode(), true)
+
+	if _, p := r.Parent(); p != nil {
+		r.AddChild("..", p, true)
+
+		ch := r.NewPersistentInode(
+			ctx, &fs.MemRegularFile{
+				Data: []byte("Hello, world!\n"),
+				Attr: fuse.Attr{
+					Mode: 0o644,
+				},
+			}, fs.StableAttr{Ino: 2})
+		r.AddChild("hello.txt", ch, false)
+	}
+}
+
+func (r *flubberRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	out.Mode = 0755
 	return 0
 }
 
-func (r *HelloRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+func (r *flubberRoot) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
 	return nil, fuse.FOPEN_KEEP_CACHE, 0
 }
 
-func (r *HelloRoot) Read(ctx context.Context, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+func (r *flubberRoot) Read(ctx context.Context, fd fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
 	data := []byte("Hello, world!\n")
 	end := off + int64(len(dest))
 	if end > int64(len(data)) {
@@ -42,22 +63,48 @@ func (r *HelloRoot) Read(ctx context.Context, dest []byte, off int64) (fuse.Read
 	return fuse.ReadResultData(data[off:end]), 0
 }
 
-var _ = (fs.NodeGetattrer)((*HelloRoot)(nil))
-var _ = (fs.NodeOnAdder)((*HelloRoot)(nil))
-var _ = (fs.NodeOpener)((*HelloRoot)(nil))
+func (r *flubberRoot) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	ops := flubberRoot{}
+	out.Mode = 0755
+	out.Size = 42
 
-//var _ = (fs.NodeReader)((*HelloRoot)(nil))
+	if name == "." {
+		return r.EmbeddedInode(), 0
+	}
+	if name == ".." {
+		if _, p := r.Parent(); p != nil {
+			return p, 0
+		}
+		return r.EmbeddedInode(), 0
+	}
 
-func Mount(dir string) error {
+	return r.NewInode(ctx, &ops, fs.StableAttr{Mode: syscall.S_IFREG}), 0
+}
+
+func (r *flubberRoot) Close(ctx context.Context) {}
+
+var (
+	// syscall access method interfaces
+	_ = (fs.NodeGetattrer)((*flubberRoot)(nil))
+	_ = (fs.NodeOnAdder)((*flubberRoot)(nil))
+	_ = (fs.NodeOpener)((*flubberRoot)(nil))
+	_ = (fs.NodeReader)((*flubberRoot)(nil))
+
+	// todo on a new file node creation
+	// _ = (fs.FileReader)((*flubberRoot)(nil))
+	// _ = (fs.FileWriter)((*flubberRoot)(nil))
+)
+
+func Mount(mountPoint string) error {
 	debug := flag.Bool("debug", true, "print debug data")
 
 	opts := &fs.Options{
 		MountOptions: fuse.MountOptions{
 			Debug: *debug,
-		}}
+		},
+	}
 
-	server, err := fs.Mount(dir, &HelloRoot{}, opts)
-
+	server, err := fs.Mount(mountPoint, &flubberRoot{}, opts)
 	if err != nil {
 		return err
 	}
