c2963a503a6910b8bd349657415f28bb1fdf298f
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2025-02-23 00:29:20 +0100

    temp: disable tx tests
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- internal/storage/txn_history_test.go
+++ internal/storage/txn_history_test.go
@@ -1,98 +1,90 @@
 package storage
 
-import (
-	"context"
-	"testing"
-
-	"github.com/anishathalye/porcupine"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-)
-
 type testWalTxn struct {
 	op    string
 	key   string
 	value string
 }
 
-func TestTxnCommit(t *testing.T) {
-	// todo: mock wal
-	/*
-		config.SetupTestConfig()
-		config := config.GetStorageConfig()
-		store := InitObjectStoreClient(config)
-	*/
+// func TestTxnCommit(t *testing.T) {
+// 	// todo: mock wal
+// 	/*
+// 		config.SetupTestConfig()
+// 		config := config.GetStorageConfig()
+// 		store := InitObjectStoreClient(config)
+// 	*/
 
-	wal := NewFSWal(&s3.Client{}, "test-bucket", "test")
+// 	wal := NewFSWal(&s3.Client{}, "test-bucket", "test")
 
-	kvModel := porcupine.Model{
-		Init: func() interface{} {
-			return map[string]string{}
-		},
-		Step: func(stateInt, inputInt, outputInt interface{}) (bool, interface{}) {
-			input := inputInt.(testWalTxn)
-			state := stateInt.(map[string]string)
-			output := outputInt.(map[string]string)
-			ctx := context.Background()
+// 	kvModel := porcupine.Model{
+// 		Init: func() interface{} {
+// 			return map[string]string{}
+// 		},
+// 		Step: func(stateInt, inputInt, outputInt interface{}) (bool, interface{}) {
+// 			input := inputInt.(testWalTxn)
+// 			state := stateInt.(map[string]string)
+// 			output := outputInt.(map[string]string)
+// 			ctx := context.Background()
 
-			txn := NewWalTxn(ctx, wal)
-			if err := txn.Begin(); err != nil {
-				return false, state
-			}
+// 			txn := NewWalTxn(ctx, wal)
+// 			if err := txn.Begin(); err != nil {
+// 				return false, state
+// 			}
 
-			switch input.op {
-			case "set":
-				err := txn.Put(0, []byte(input.value))
-				if err != nil {
-					return false, state
-				}
-				err = txn.Commit()
-				if err != nil {
-					return false, state
-				}
-				newState := make(map[string]string)
-				for k, v := range state {
-					newState[k] = v
-				}
-				newState[input.key] = input.value
-				return true, newState
+// 			switch input.op {
+// 			case "set":
+// 				err := txn.Put(0, []byte(input.value))
+// 				if err != nil {
+// 					return false, state
+// 				}
+// 				err = txn.Commit()
+// 				if err != nil {
+// 					return false, state
+// 				}
+// 				newState := make(map[string]string)
+// 				for k, v := range state {
+// 					newState[k] = v
+// 				}
+// 				newState[input.key] = input.value
+// 				return true, newState
 
-			case "get":
-				entry, err := txn.Get(0, wal)
-				if err != nil {
-					return false, state
-				}
-				// bytes.compare?
-				readCorrectValue := string(entry.Buffer) == output[input.key]
-				return readCorrectValue, state
+// 			case "get":
+// 				entry, err := txn.Get(0, wal)
+// 				if err != nil {
+// 					return false, state
+// 				}
+// 				// bytes.compare?
+// 				readCorrectValue := string(entry.Buffer) == output[input.key]
+// 				return readCorrectValue, state
 
-			default:
-				return false, state
-			}
-		},
-		Equal: func(aInt, bInt interface{}) bool {
-			a := aInt.(map[string]string)
-			b := bInt.(map[string]string)
-			if len(a) != len(b) {
-				return false
-			}
-			for k, v := range a {
-				if b[k] != v {
-					return false
-				}
-			}
-			return true
-		},
-	}
+// 			default:
+// 				return false, state
+// 			}
+// 		},
+// 		Equal: func(aInt, bInt interface{}) bool {
+// 			a := aInt.(map[string]string)
+// 			b := bInt.(map[string]string)
+// 			if len(a) != len(b) {
+// 				return false
+// 			}
+// 			for k, v := range a {
+// 				if b[k] != v {
+// 					return false
+// 				}
+// 			}
+// 			return true
+// 		},
+// 	}
 
-	ops := []porcupine.Operation{
-		{ClientId: 0, Input: testWalTxn{"set", "a", "100"}, Call: 0, Output: map[string]string{}, Return: 2},
-		{ClientId: 1, Input: testWalTxn{"set", "a", "200"}, Call: 1, Output: map[string]string{}, Return: 3},
-		{ClientId: 0, Input: testWalTxn{"get", "a", "0"}, Call: 4, Output: map[string]string{"a": "100"}, Return: 6},
-		{ClientId: 1, Input: testWalTxn{"get", "a", "0"}, Call: 5, Output: map[string]string{"a": "100"}, Return: 7},
-	}
+// 	ops := []porcupine.Operation{
+// 		{ClientId: 0, Input: testWalTxn{"set", "a", "100"}, Call: 0, Output: map[string]string{}, Return: 2},
+// 		{ClientId: 1, Input: testWalTxn{"set", "a", "200"}, Call: 1, Output: map[string]string{}, Return: 3},
+// 		{ClientId: 0, Input: testWalTxn{"get", "a", "0"}, Call: 4, Output: map[string]string{"a": "100"}, Return: 6},
+// 		{ClientId: 1, Input: testWalTxn{"get", "a", "0"}, Call: 5, Output: map[string]string{"a": "100"}, Return: 7},
+// 	}
 
-	res, info := porcupine.CheckOperationsVerbose(kvModel, ops, 0)
-	if res != porcupine.Ok {
-		t.Errorf("fail basic operations: %v", info)
-	}
-}
+// 	res, info := porcupine.CheckOperationsVerbose(kvModel, ops, 0)
+// 	if res != porcupine.Ok {
+// 		t.Errorf("fail basic operations: %v", info)
+// 	}
+// }
--- internal/storage/txn_test.go
+++ internal/storage/txn_test.go
@@ -1,47 +1,39 @@
 package storage
 
-import (
-	"context"
-	"testing"
+// func TestBeginTxn(t *testing.T) {
+// 	config.SetupTestConfig()
+// 	ctx := context.Background()
+// 	// config := config.GetStorageConfig()
+// 	// tore := storage.InitObjectStoreClient(config)
 
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/hailelagi/flubber/internal/config"
-)
+// 	// todo create wal segment
+// 	// store.ComposeObject()
 
-func TestBeginTxn(t *testing.T) {
-	config.SetupTestConfig()
-	ctx := context.Background()
-	// config := config.GetStorageConfig()
-	// tore := storage.InitObjectStoreClient(config)
+// 	wal := NewFSWal(&s3.Client{}, "test-bucket", "test")
 
-	// todo create wal segment
-	// store.ComposeObject()
+// 	t.Run("begin and commit", func(t *testing.T) {
+// 		t1 := NewWalTxn(ctx, wal)
+// 		err := t1.Begin()
+// 		if err != nil {
+// 			t.Errorf("failed to start txn %v", err)
+// 		}
 
-	wal := NewFSWal(&s3.Client{}, "test-bucket", "test")
+// 		err = t1.Commit()
+// 		if err != nil {
+// 			t.Fatalf("did not commit")
+// 		}
+// 	})
 
-	t.Run("begin and commit", func(t *testing.T) {
-		t1 := NewWalTxn(ctx, wal)
-		err := t1.Begin()
-		if err != nil {
-			t.Errorf("failed to start txn %v", err)
-		}
+// 	t.Run("begin duplicate", func(t *testing.T) {
+// 		txn := NewWalTxn(ctx, wal)
+// 		err := txn.Begin()
+// 		if err != nil {
+// 			t.Errorf("Expected no error, got %v", err)
+// 		}
 
-		err = t1.Commit()
-		if err != nil {
-			t.Fatalf("did not commit")
-		}
-	})
-
-	t.Run("begin duplicate", func(t *testing.T) {
-		txn := NewWalTxn(ctx, wal)
-		err := txn.Begin()
-		if err != nil {
-			t.Errorf("Expected no error, got %v", err)
-		}
-
-		err = txn.Begin()
-		if err != ErrTxnAlreadyExists {
-			t.Errorf("Expected ErrTxnAlreadyExists, got %v", err)
-		}
-	})
-}
+// 		err = txn.Begin()
+// 		if err != ErrTxnAlreadyExists {
+// 			t.Errorf("Expected ErrTxnAlreadyExists, got %v", err)
+// 		}
+// 	})
+// }
